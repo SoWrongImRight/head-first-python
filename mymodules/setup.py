06aae37475f3ec01361e5c3a426a08eea84e6fdc
SoWrongImRight <58298347+SoWrongImRight@users.noreply.github.com>
2020-10-06 19:27:36 -0400

    Adding files for first module, vsearch

---
+++ mymodules/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup (
+    name='vsearch',
+    version='1.0',
+    description="Head First Python Search Tools",
+    author='Russ Carroll',
+    author_email="dev.ops.rc.1980@gmail.com",
+    py_modules=['vsearch']
+)
